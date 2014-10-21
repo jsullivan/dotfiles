eb888b70bbebcacb50849d3602f1dec7e8ba3a3e
Josh Sullivan <josh@dipperstove.com>
2014-10-21 14:30:17 -0700

    Add tools and apps

---
+++ install/apps.sh
@@ -0,0 +1,30 @@
+ok brew
+ok brew brew-cask --from=caskroom/cask
+
+# work
+ok cask iterm2
+ok cask slack
+ok cask flowdock
+ok cask hipchat
+ok cask mailbox
+ok cask postgres
+
+# security and backups
+ok cask onepassword
+ok cask dropbox
+ok cask backblaze
+
+ok cask rdio
+#ok cask kindle
+ok cask adobe-creative-cloud
+ok cask minecraft
+ok cask google-chrome
+#ok cask monosnap
+ok cask marked
+ok cask vagrant
+ok cask virtualbox
+#ok cask twitter
+ok cask vlc
+
+ok macstore monosnap 540348655
+ok macstore twitter 333903271
---
+++ install/tools.sh
@@ -0,0 +1,9 @@
+ok brew
+
+ok brew git
+ok brew tig
+ok brew hub
+ok brew vim
+ok brew tmux
+ok brew wget
+ok brew rbenv
