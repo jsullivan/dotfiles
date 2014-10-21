9f6ea1d5936a7717914836d1f0cc55503786618a
Josh Sullivan <josh@dipperstove.com>
2014-10-21 13:36:09 -0700

    Symlink dotfiles

--- install/dotfiles.sh
+++ install/dotfiles.sh
@@ -1,8 +1,9 @@
-ok directory $HOME/code/
-destination $HOME/code/
-ok github jsullivan/dotfiles
+# Ensure all dotfiles are symlinked
+ok symlink .gitconfig $HOME/code/dotfiles/configs/.gitconfig
+ok symlink .gitignore $HOME/code/dotfiles/configs/.gitignore
+ok symlink .tmux.conf $HOME/code/dotfiles/configs/.tmux.conf
+ok symlink .vim $HOME/code/dotfiles/configs/.vim
+ok symlink .vimrc $HOME/code/dotfiles/configs/.vimrc
+ok symlink .zprofile $HOME/code/dotfiles/configs/.zprofile
+ok symlink .zshrc $HOME/code/dotfiles/configs/.zshrc
 
-destination $HOME
-for config in $HOME/code/dotfiles/configs/*; do
-  ok symlink "$HOME/.$(basename $config)" $config
-done
