c466e441aa0dfb27680bdb4a89da4c5de6469f8a
Josh Sullivan <josh@dipperstove.com>
2015-03-18 15:20:13 -0700

    Add moar tools

--- install/tools.sh
+++ install/tools.sh
@@ -7,8 +7,14 @@ ok brew vim
 ok brew tmux
 ok brew wget
 ok brew rbenv
+ok brew ruby-build
 ok brew heroku-toolbelt
 ok brew tmate --from=nviennot/tmate
+ok brew ag
+ok brew redis
+ok cask gpgtools
+ok brew go
+ok brew node
 
 ok brew gcc # This compiler is necessary for mysql51
 ok brew mysql51
