cba1ee65e750bd8a785d83a05cb91e9fb1a24e3f
Josh Sullivan <josh@dipperstove.com>
2018-03-12 15:46:14 -0700

    simplify dotfile symlinking

--- install/dotfiles.sh
+++ install/dotfiles.sh
@@ -1,10 +1,7 @@
 # Ensure all dotfiles are symlinked
 # Run this with bork from your home dir like so:
 # `bork satisfy code/dotfiles/install/dotfiles.sh`
-ok symlink .gitconfig $HOME/code/dotfiles/configs/.gitconfig
-ok symlink .gitignore $HOME/code/dotfiles/configs/.gitignore
-ok symlink .tmux.conf $HOME/code/dotfiles/configs/.tmux.conf
-ok symlink .vim $HOME/code/dotfiles/configs/.vim
-ok symlink .vimrc $HOME/code/dotfiles/configs/.vimrc
-ok symlink .zprofile $HOME/code/dotfiles/configs/.zprofile
-ok symlink .zshrc $HOME/code/dotfiles/configs/.zshrc
+for file in $HOME/dotfiles/configs/.[!.]*
+do
+  ok symlink "$(basename $file)" $file
+done
